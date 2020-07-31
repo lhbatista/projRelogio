fbfe9f462d53ed6a151942aac958b59d5ba21ee4
luiis_henrique@hotmail.com <luiis_henrique@hotmail.com>
2020-07-31 16:39:27 -0300

    [ADD] First Commit

---
+++ Hora.java
@@ -0,0 +1,9 @@
+public class Hora {
+    public static void main(String[] args) {
+        
+
+
+
+    }
+    
+}
---
+++ Relogio.java
@@ -0,0 +1,20 @@
+public class Relogio {
+
+    //Atributos
+    int hour;
+    int min;
+    int sec;
+
+    //Método construtor
+    public Relogio (int hour, int min, int sec){
+        this.hour = hour;
+        this.min = min;
+        this.sec = sec;
+    }
+    
+
+    //Método da classe
+    public void getHora(){
+    
+    }
+}
